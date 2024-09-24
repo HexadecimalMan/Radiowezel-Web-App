285b5e6e04fde2c69ed75081d67ea2d4c052ae0c
HexadecimalMan <morgasmaciej@gmail.com>
2024-09-24 23:33:19 +0200

    Changed appearence

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,8 @@ const date = new Date()
 
 function Footer() {
     return (
-        <footer class="container text-start mt-5 pt-5">
-            <p style={{fontSize: 0.75+'rem'}}>&copy; 2022-{date.getFullYear()} Radiowęzeł VIII Liceum Ogólnokształcącego im. KEN w Gdańsku&nbsp;&nbsp;&nbsp;&bull;&nbsp;&nbsp;&nbsp;Made by Maciej Morgaś & Maciej Tkaczewski</p>
+        <footer class="bg-dark text-start">
+            <p class="m-2" style={{fontSize: 0.6+'rem'}}>&copy; 2022-{date.getFullYear()} Radiowęzeł VIII Liceum Ogólnokształcącego im. KEN w Gdańsku&nbsp;&nbsp;&nbsp;&bull;&nbsp;&nbsp;&nbsp;Made by Maciej Morgaś & Maciej Tkaczewski</p>
         </footer>
     );
 }
