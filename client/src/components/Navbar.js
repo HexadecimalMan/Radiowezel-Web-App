022ecf627f947f2ef30535842cce9de15ecbca9d
HexadecimalMan <morgasmaciej@gmail.com>
2024-10-21 23:43:46 +0200

    Made responsive to scrolling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link, useLocation } from "react-router-dom";
-import logo from './logo.png'
+
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import { useState, useEffect } from 'react';
+import "bootstrap/js/src/collapse.js";
 
-
-const linkStyle = {
-  textDecoration: "none"
-};
+import logo from './logo.png'
+import './background.css'
 
 function Navbar() {
   const [navLinksStyle, updateNavLinksStyle] = useState({
@@ -19,6 +17,26 @@ function Navbar() {
     about: '',
     schedule: ''
   })
+
+  const [background, changeBackground] = useState(false);
+  function updateBackground() {
+    changeBackground(true)
+  }
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+      const position = window.scrollY;
+      console.log(position)
+      setScrollPosition(position);
+  };
+
+  useEffect(() => {
+      window.addEventListener('scroll', handleScroll, { passive: true });
+
+      return () => {
+          window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
 
   let location = useLocation();
 
@@ -82,12 +100,12 @@ function Navbar() {
 
     return (
         <header data-bs-theme="dark">
-          <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <nav class={`navbar navbar-expand-md navbar-dark fixed-top ${background ? "bg-dark" : ""}`} style={{backgroundColor: `rgba(33,37,41,${scrollPosition / 200})`}}>
               <div class="container-fluid">
                   <a class="navbar-brand" href="/">
                       <img style={{height: 3+'rem'}} src={logo}></img>
                   </a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                  <button class="navbar-toggler" type="button" onClick={updateBackground} data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation" hidden={background}>
                       <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarCollapse">
